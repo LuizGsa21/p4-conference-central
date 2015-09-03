c55fa7aeb24657f7ef8386d5ff592f2f74e2ea60
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-09-02 19:23:53 -0500

    Add createSession test

--- test/test_conference.py
+++ test/test_conference.py
@@ -1,3 +1,4 @@
+from endpoints import UnauthorizedException, ForbiddenException, get_current_user
 from base import BaseEndpointAPITestCase
 from google.appengine.api import users
 from google.appengine.api import memcache
@@ -8,6 +9,7 @@ from google.appengine.ext import testbed
 from conference import (
     ConferenceApi,
     CONF_GET_REQUEST,
+    SESSION_POST_REQUEST,
     SESSION_BY_TYPE_GET_REQUEST,
     SESSION_BY_SPEAKER_GET_REQUEST
 
@@ -99,3 +101,52 @@ class ConferenceTestCase(BaseEndpointAPITestCase):
         assert r_sessions[0].speaker == 'superman', 'returned an invalid session'
 
         print 'Successfully return all sessions by a particular speaker'
+
+    def testCreateSession(self):
+        self.initDatabase()
+        conf = Conference.query(Conference.name == 'room #1').fetch(1)[0]
+
+        # fill out session form fields in SESSION_POST_REQUEST
+        sessionFields = {
+            'name': 'Computer programming',
+            'speaker': 'Donald Knuth',
+            'typeOfSession': 'educational',
+            'date': '2015-09-10',
+            'startTime': '11:00',
+            'duration': 100
+        }
+        container = SESSION_POST_REQUEST.combined_message_class(
+            websafeConferenceKey=conf.key.urlsafe(),
+            **sessionFields
+        )
+
+        # get current session count
+        initialCount = conf.sessions.count()
+
+        # Attempt to add a session without being logged in
+        try:
+            r = self.api.createSession(container)
+            assert False, 'UnauthorizedException should of been thrown...'
+        except UnauthorizedException:
+            pass
+        # make sure a session wasn't added
+        count = conf.sessions.count()
+        assert count == initialCount, 'Only the organizer of the conference may create sessions'
+
+        # Attempt to add a session with a logged in but unauthorized user.
+        self.login(email='test2@test.com')
+        try:
+            r = self.api.createSession(container)
+            assert False, 'ForbiddenException should of been thrown...'
+        except ForbiddenException:
+            pass
+        # make sure a session wasn't added
+        count = conf.sessions.count()
+        assert count == initialCount, 'Only the organizer of the conference may create sessions'
+
+        # Finally, add session using the authorized user
+        self.login(email=conf.organizerUserId)
+        r = self.api.createSession(container)
+        count = conf.sessions.count()
+        assert count == initialCount + 1, 'Failed to add session to conference'
+        print 'Successfully created a session open to the organizer of the conference'
